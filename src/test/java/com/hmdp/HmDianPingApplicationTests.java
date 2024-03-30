8c8e76ffa096af776800bf05c80844dc999da650
PM2.6 <bijibao@123.com>
2024-03-30 20:21:01 +0800

    redis自增 实现全局唯一ID

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -3,10 +3,15 @@ package com.hmdp;
 import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
@@ -19,6 +24,12 @@ class HmDianPingApplicationTests {
 
 @Resource
 private CacheClient cacheClient;
+
+@Autowired
+private RedisIdWorker redisIdWorker;
+
+
+private ExecutorService executorService= Executors.newFixedThreadPool(500);
 @Test
     void testShopSave() throws InterruptedException {
     shopService.saveShopToRedis(1L,10L);
@@ -30,5 +41,33 @@ private CacheClient cacheClient;
 
     Shop shop = shopService.getById(1L);
     cacheClient.setWithLogicalExpire(CACHE_SHOP_KEY+1L,shop,10L, TimeUnit.SECONDS);
+}
+
+@Test
+    void testIdWorker() throws InterruptedException {
+    CountDownLatch countDownLatch=new CountDownLatch(300);
+
+    Runnable task=()->{
+        for (int i=0;i<100;i++)
+        {
+
+            Long id = redisIdWorker.nextId("orders");
+            System.out.println("id="+id);
+        }
+        countDownLatch.countDown();
+    };
+
+    long begin=System.currentTimeMillis();
+    for (int i=0;i<300;i++)
+    {
+
+        executorService.submit(task);
+    }
+
+    countDownLatch.await();
+
+    long end=System.currentTimeMillis();
+
+    System.out.println("time = "+(end-begin));
 }
 }
